fb79f732c5c01874b46bc002132d5572ce1d800b
Maru\maru_7717 <734886783@qq.com>
2023-05-11 10:32:49 +0800

    完成优惠券秒杀的基本解决方案 使用乐观锁

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -17,4 +26,59 @@ import org.springframework.stereotype.Service;
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+    @Resource
+    ISeckillVoucherService seckillVoucherService;
+
+    @Resource
+    RedisIdWorker redisIdWorker;
+
+    @Override
+    @Transactional
+    public Result seckillVoucher(Long voucherId) {
+        //查询优惠券
+        SeckillVoucher seckillVoucher = seckillVoucherService.getById(voucherId);
+
+        //判断是否开始和结束
+
+        if (seckillVoucher.getBeginTime().isAfter(LocalDateTime.now())){
+            return Result.fail("秒杀尚未开始");
+        }
+
+        if(seckillVoucher.getEndTime().isBefore(LocalDateTime.now())){
+            return Result.fail("秒杀已经结束");
+        }
+        //判断是否库存充足
+
+        if(seckillVoucher.getStock()<1){
+            return Result.fail("秒杀券库存不足");
+        }
+
+        //扣减库存
+        boolean success = seckillVoucherService.update()
+                .setSql("stock = stock - 1")
+                .eq("voucher_id",voucherId)
+                .gt("stock",0) //加一行 stock = 刚开始查到的stock 确定没有被其他线程修改过 才进行更新
+                .update();
+
+        if(!success){
+            return Result.fail("扣减失败 库存不足");
+        }
+
+        //创建订单
+
+        VoucherOrder voucherOrder = new VoucherOrder();
+
+        Long orderId = redisIdWorker.nextId("order");
+        voucherOrder.setId(orderId);
+
+        Long userId = UserHolder.getUser().getId();
+        voucherOrder.setUserId(userId);
+
+        voucherOrder.setVoucherId(voucherId);
+
+        save(voucherOrder);
+
+        //返回订单id
+        return Result.ok(orderId );
+     }
 }
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,54 @@
 package com.hmdp;
 
+import cn.hutool.json.JSONUtil;
+import com.hmdp.entity.Voucher;
+import com.hmdp.utils.RedisIdWorker;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
+
+    @Resource
+    RedisIdWorker redisIdWorker;
+
+    private ExecutorService es = Executors.newFixedThreadPool(500);
+
+    @Test
+    void testIdWorker(){
+//         Runnable task = ()->{
+//             for(int i =0 ; i < 100; i++){
+//                 long id = redisIdWorker.nextId("order");
+//                 System.out.println("id = " + id);
+//             }
+//         };
+//        System.out.println("time start");
+//         es.submit(task);
+//        System.out.println("time end");
+
+        System.out.println("long id = " + redisIdWorker.nextId("order"));
+
+    }
+
+    @Test
+    void voucherJsonTest(){
+        Voucher voucher = new Voucher();
+        voucher.setShopId(1L);
+        voucher.setTitle("100元代金券");
+        voucher.setSubTitle("周一到周五均可使用");
+        voucher.setRules("全场通用 无需预约 仅限堂食");
+        voucher.setPayValue(8000L);
+        voucher.setActualValue(10000l);
+        voucher.setType(1);
+        voucher.setStock(100);
+
+        System.out.println(JSONUtil.toJsonStr(voucher));
+    }
 
 
 }
